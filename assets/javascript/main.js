a947c97273b2bb9547d746c6fffa067f0cfac23b
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-07-11 00:32:25 -0700

    project styling updated

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,42 +7,49 @@ let data = {
         photo: "assets/images/gallows.jpg",
         altname: "The Word Guess Game",
         link: "https://kthoma2484.github.io/word-guess-game/",
+        order: "#one!",
     },
     {
         name: "The Psychic Game", 
         photo: "assets/images/113458-Crystal-Ball.jpg",
         altname: "The Psychic Game",
         link: "https://kthoma2484.github.io/psychic-game/",
+        order: "#two!",
     },
     {
         name: "Crystal Collection Game", 
         photo: "assets/images/crystals.jpg",
         altname: "Crystal Collection Game",
         link: "https://kthoma2484.github.io/unit-4-game-crystal-crazy/",
+        order: "#three!",
     },
     {
         name: "Giftastic - 90's Artists!", 
         photo: "assets/images/90srnb.jpg",
         altname: "Giftastics - 90s artists",
         link: "https://kthoma2484.github.io/giftastic-90sartists/",
+        order: "#four!",
     },
     {
         name: "TMNT Battle Royale", 
         photo: "assets/images/TMNT.jpg",
         altname: "TMNT Battle Royale",
         link: "https://kthoma2484.github.io/unit-4-game-tmnt-battleroyale-game/",
+        order: "#five!",
     },
     {
         name: "Train Scheduler", 
         photo: "assets/images/space train.jpg",
         altname: "Train Scheduler",
         link: "https://kthoma2484.github.io/train-scheduler/",
+        order: "#six!",
     },
     {
         name: "Dessert Crave", 
         photo: "assets/images/dessertssign.jpg",
         altname: "Dessert Crave",
         link: "https://erickagithub.github.io/project-1/",
+        order: "#seven!",
     }]
 };
     
@@ -61,4 +68,7 @@ let data = {
 
     createHTML(data);
 
+    $('.carousel').carousel();
+
+    
 });
